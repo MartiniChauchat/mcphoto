c5928b53ea99f37b14f9ec9dfe66dc43c0eab4b3
CharlesLyu <zikun.lyu@mail.mcgill.ca>
2020-09-13 17:31:19 -0400

    format profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import './profile/common-css/fluidbox.min.css';
 import './profile/css/styles.css';
 import axios from 'axios';
+import profile from '../images/profile.jpg'
 
 
 export default class Profile extends Component {
@@ -87,14 +88,14 @@ render() {
                         <div class="col-md-10 col-lg-8">
                             <div class="intro">
                                 <div class="profile-img">
-                                    <img src="/images/portfolio-4-600x400.jpg" alt="" />
+                                    <img src={profile} alt=""/>
                                 </div>
                                 <h2><b>{this.state.user.name}</b></h2>
-                                <h4 class="font-yellow">Student</h4>
+                                <h4 class="font-yellow">McGill Student</h4>
                                 <ul class="information margin-tb-30">
                                     <li><b>BORN : </b>August 25, 1987</li>
-                                    <li><b>EMAIL : </b>mymith@mywebpage.com</li>
-                                    <li><b>MARITAL STATUS : </b>Married</li>
+                                    <li><b>EMAIL : </b>{this.state.user.email}</li>
+                                    <li><b>Age : </b>{this.state.user.age}</li>
                                 </ul>
                                 <ul class="social-icons">
                                     <li><a href="#"><i class="ion-social-pinterest"></i></a></li>
@@ -108,6 +109,109 @@ render() {
                     </div>
                 </div>
             </section>
+
+            <section class="about-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>About me</b></h3>
+						<h6 class="font-lite-black"><b>PROFESSIONAL PATH</b></h6>
+					</div>
+				</div>
+				<div class="col-sm-8">
+					<p class="margin-b-50">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque 
+					sit amet sodales. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+					Nulla maximus pellentes que velit, quis consequat nulla effi citur at. 
+					Maecenas sed massa tristique.Duis non volutpat arcu, eu mollis tellus. 
+					Sed finibus aliquam neque sit amet sodales. Lorem ipsum dolor sit amet, 
+					consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur 
+					adipiscing elit. Nulla maximus pellentes que velit, quis consequat nulla 
+					effi citur at.Maecenas sed massa tristique.</p>
+					
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <section class="experience-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Work Experience</b></h3>
+						<h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>
+					</div>
+				</div>
+				<div class="col-sm-8">
+				
+					<div class="experience margin-b-50">
+						<h4><b>JUNIOR PROJECT MANAGER</b></h4>
+						<h5 class="font-yellow"><b>DESIGN STUDIO</b></h5>
+						<h6 class="margin-t-10">MARCH 2015 - PRESENT</h6>
+						<p class="font-semi-white margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
+							quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
+							eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+						<ul class="list margin-b-30">
+							<li>Duis non volutpat arcu, eu mollis tellus.</li>
+							<li>Quis consequat nulla effi citur at.</li>
+							<li>Sed finibus aliquam neque sit.</li>
+						</ul>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+    <section class="education-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Education</b></h3>
+						<h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>
+					</div>
+				</div>
+				<div class="col-sm-8">
+					<div class="education-wrapper">
+						<div class="education margin-b-50">
+							<h4><b>MASTER DEGREE IN SCIENCE</b></h4>
+							<h5 class="font-yellow"><b>UCLA - SCIENCE AND ENGINEERING</b></h5>
+							<h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2010(2 YEARS)</h6>
+							<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
+							quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
+							eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+						</div>
+						
+						<div class="education margin-b-50">
+							<h4><b>COURSE ON COMPUTER SCIENCE</b></h4>
+							<h5 class="font-yellow"><b>NEW YORK PUBLIC UNIVERSITY</b></h5>
+							<h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2009(6 MONTHS)</h6>
+							<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
+							quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
+							eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+						</div>
+						
+						<div class="education margin-b-50">
+							<h4><b>GRADUATED VALEDICTERIAN</b></h4>
+							<h5 class="font-yellow"><b>PUBLIC COLLEGE</b></h5>
+							<h6 class="font-lite-black margin-t-10">GRADUATED IN MAY 2008(4 YEARS)</h6>
+							<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
+							quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
+							eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</section>
 
 
         </div>
